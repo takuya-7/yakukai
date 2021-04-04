99c8f33cf8497d1f41fd9a1531135fe36682ed2f
takuya-7 <ts.procd@gmail.com>
2021-04-04 18:18:21 +0900

    トップページからの検索機能実装
    トップページから検索可能にするため

--- index.php
+++ index.php
@@ -75,99 +75,96 @@ require('head.php');
           </div>
   
           <div class="top-search-box">
-            <form method="get" action="">
-              <input class="" type="text" placeholder="企業名で検索" name="searchCompanyName">
+            <form method="get" action="search.php">
+              <input class="" type="text" placeholder="企業名で検索" name="src_str" value="<?php if(!empty($_GET['src_str'])) echo $_GET['src_str']; ?>">
               <button class="" type="submit">検索</button>
             </form>
           </div>
         </div>
       </div>
       
-      <div class="container">
-        <section class="top-widget natural-shadow">
-          <div class="top-widget-title">
-            <h2>人気の企業</h2>
-          </div>
-          <div></div>
-        </section>
-
-        <section class="top-widget natural-shadow">
-          <div class="top-widget-title">
-            <h2>エリア・業種から口コミを探す</h2>
-          </div>
-          <div class="select-box">
-            <form method="get" action="search.php">
-              <div class="select-box-items">
-                <div class="select-box-item">
-                  <div class="cp_ipselect cp_sl01">
-                    <select name="pref">
-                    <option value="0" <?php if(getFormData('pref', true) == 0) echo 'selected'; ?>>都道府県</option>
-                      <?php
-                        foreach($dbPrefectureData as $key => $val){
-                      ?>
-                        <option value="<?php echo $val['id']; ?>" <?php if(getFormData('pref', true) == $val['id']) echo 'selected'; ?>>
-                          <?php echo $val['name']; ?>
-                        </option>
-                      <?php
-                        }
-                      ?>
-                    </select>
+      <div class="content-wrapper">
+        <div class="container">
+          <section class="top-widget natural-shadow">
+            <div class="top-widget-title">
+              <h2>人気の企業</h2>
+            </div>
+            <div></div>
+          </section>
+  
+          <section class="top-widget natural-shadow">
+            <div class="top-widget-title">
+              <h2>エリア・業種から口コミを探す</h2>
+            </div>
+            <div class="select-box">
+              <form method="get" action="search.php">
+                <div class="select-box-items">
+                  <div class="select-box-item">
+                    <div class="cp_ipselect cp_sl01">
+                      <select name="pref">
+                      <option value="0" <?php if(getFormData('pref', true) == 0) echo 'selected'; ?>>都道府県</option>
+                        <?php
+                          foreach($dbPrefectureData as $key => $val){
+                        ?>
+                          <option value="<?php echo $val['id']; ?>" <?php if(getFormData('pref', true) == $val['id']) echo 'selected'; ?>>
+                            <?php echo $val['name']; ?>
+                          </option>
+                        <?php
+                          }
+                        ?>
+                      </select>
+                    </div>
+                  </div>
+    
+                  <div class="select-box-item-x">
+                    ×
+                  </div>
+    
+                  <div class="select-box-item">
+                    <div class="cp_ipselect cp_sl01">
+                      <select name="i">
+                        <option value="0" <?php if(getFormData('i', true) == 0) echo 'selected'; ?>>業種</option>
+  
+                        <?php
+                          foreach($dbIndustryData as $key => $val){
+                        ?>
+                          <option value="<?php echo $val['id']; ?>" <?php if(getFormData('i', true) == $val['id']) echo 'selected'; ?>>
+                            <?php echo $val['name']; ?>
+                          </option>
+                        <?php
+                          }
+                        ?>
+                      </select>
+                    </div>
                   </div>
                 </div>
   
-                <div class="select-box-item-x">
-                  ×
-                </div>
+                <button type="submit">
+                  この条件で口コミを探す
+                </button>
+              </form>
+            </div>
+          </section>
+    
+          <section class="top-widget natural-shadow">
+            <h2>ヤクカイ - 薬剤師のためのキャリア会議</h2>
+            <div class="content">
+              <p>ヤクカイは、薬剤師同士が会社クチコミ・仕事経験を共有することで就職・転職時のより良い選択を応援します。</p>
+            </div>
+          </section>
   
-                <div class="select-box-item">
-                  <div class="cp_ipselect cp_sl01">
-                    <select name="i">
-                      <option value="0" <?php if(getFormData('i', true) == 0) echo 'selected'; ?>>業種</option>
-
-                      <?php
-                        foreach($dbIndustryData as $key => $val){
-                      ?>
-                        <option value="<?php echo $val['id']; ?>" <?php if(getFormData('i', true) == $val['id']) echo 'selected'; ?>>
-                          <?php echo $val['name']; ?>
-                        </option>
-                      <?php
-                        }
-                      ?>
-                    </select>
-                  </div>
-                </div>
-              </div>
-
-              <button type="submit">
-                この条件で口コミを探す
+          <section class="signup-wrap">
+            <div class="buttons">
+              <button class="signup" onclick="location.href='signup.php'">
+                ユーザー登録する（無料）
               </button>
-            </form>
-          </div>
-        </section>
-  
-        <section class="top-widget natural-shadow">
-          <h2>ヤクカイ - 薬剤師のためのキャリア会議</h2>
-          <div class="content">
-            <p>ヤクカイは、薬剤師同士が会社クチコミ・仕事経験を共有することで就職・転職時のより良い選択を応援します。</p>
-          </div>
-        </section>
-
-        <section class="top-widget">
-          <div class="buttons">
-            <button class="signup" onclick="location.href='signup.php'">
-              ユーザー登録する（無料）
-            </button>
-            <button class="login" onclick="location.href='login.php'">
-              ログインする
-            </button>
-          </div>
-        </section>
+              <button class="login" onclick="location.href='login.php'">
+                ログインする
+              </button>
+            </div>
+          </section>
+        </div>
       </div>
-
-
-        
-      
-
     </main>
 
     <!-- footer -->
