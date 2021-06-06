1ba9d13cf315d402393172bc7c6484c00fcb4378
takuya-7 <ts.procd@gmail.com>
2021-06-06 20:59:35 +0900

    style.css読み込み先変更
    Sass導入でディレクトリ構造変更による

--- head.php
+++ head.php
@@ -7,20 +7,10 @@
 
     <title><?php echo $siteTitle; ?> | ヤクカイ</title>
 
-    <!-- BootstrapのCSS読み込み -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- jQuery読み込み -->
-    <script src="js/vender/jquery-3.5.1.min.js"></script>
-    <!-- BootstrapのJS読み込み -->
-    <script src="js/bootstrap.min.js"></script>
-    <!-- style読み込み -->
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" type="text/css" href="responsive.css">
+    <!-- style.css（reset.css, bootstrap等全て入れて圧縮したもの）読み込み -->
+    <link rel="stylesheet" type="text/css" href="dist/scss/style.css">
     <!-- Font Awsome読み込み -->
     <!-- <script src="https://kit.fontawesome.com/7f36980d49.js" crossorigin="anonymous"></script> -->
-
-    <!-- Bootstrap CSS -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"> -->
 
     <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
