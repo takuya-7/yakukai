62270a720619f0e04a60b241ea1a165eb16f1fe6
takuya-7 <ts.procd@gmail.com>
2021-06-18 14:37:00 +0900

    class名変更、構造修正
    FLOCSS導入、レスポンシブ対応のため

--- category.php
+++ category.php
@@ -61,94 +61,100 @@ require('head.php');
     <div class="l-content-wrapper">
       <div class="l-container">
 
-        <div class="mb-3">
+        <div class="u-mb-3">
           <a href="surveyInfo.php" class="c-button c-button--blue c-button--width100">クチコミを投稿する</a>
         </div>
 
-        <div class="company-wrapper">
-          <div class="company-heading">
-            <section>
-              <h1><?php echo $dbCompanyData['info']['name']; ?><span class="company-title-append">のクチコミ・評判・年収</span></h1>
-              <div class="head-rating mb-4">
-                <span class="level-of-well-being">幸福度</span>
-                <br>
-                <span class="heart5_rating" data-rate="<?php echo round($dbCompanyRatings[0]['AVG(rating)'], 1); ?>"></span>
-                <span class="rating-val"><?php echo round($dbCompanyRatings[0]['AVG(rating)'], 1); ?></span>
-                <span class="answer-count">（回答：<?php echo $dbCompanyRatings['rating_count']['COUNT(rating)']; ?>件）</span>
+        <div class="l-content p-company-head">
+          <section>
+            <h1 class="c-page-title"><?php echo $dbCompanyData['info']['name']; ?><span class="company-title-append">のクチコミ・評判・年収</span></h1>
+            <div class="head-rating u-mb-4">
+              <span class="level-of-well-being">幸福度</span>
+              <br>
+              <span class="heart5_rating" data-rate="<?php echo round($dbCompanyRatings[0]['AVG(rating)'], 1); ?>"></span>
+              <span class="rating-val"><?php echo round($dbCompanyRatings[0]['AVG(rating)'], 1); ?></span>
+              <span class="answer-count">（回答：<?php echo $dbCompanyRatings['rating_count']['COUNT(rating)']; ?>件）</span>
+            </div>
+
+            <div class="l-company-head">
+              <div class="l-company-head__chart">
+                <canvas id="header-chart"></canvas>
               </div>
-
-              <canvas id="header-chart"></canvas>
-
-              <div class="main-params-summary row text-center mb-4">
-                <div class="col-6 border-end">
-                  <div class="title">
-                    平均年収<br>（正社員薬剤師）
+  
+              <div class="l-company-head__info">
+                <div class="main-params-summary row u-text-center u-mb-4">
+                  <div class="col-6 border-end">
+                    <div class="title">
+                      平均年収<br>（正社員薬剤師）
+                    </div>
+                    <div class="u-mb-2">
+                      <span class="param"><?php echo round($dbCompanyData['user_data']['AVG(anual_total_salary)'], 0); ?></span>
+                      <span> 万円</span>
+                    </div>
+                    <div>
+                      <span>（回答：<?php echo $dbCompanyData['user_data']['COUNT(anual_total_salary)']; ?>件）</span>
+                    </div>
                   </div>
-                  <div class="mb-2">
-                    <span class="param"><?php echo round($dbCompanyData['user_data']['AVG(anual_total_salary)'], 0); ?></span>
-                    <span> 万円</span>
+                  <div class="col-6">
+                    <div class="title">
+                      平均残業時間<br>（月間）
+                    </div>
+                    <div class="u-mb-2">
+                      <span class="param"><?php echo round($dbCompanyData['user_data']['AVG(over_time)'], 0); ?></span>
+                      <span> 時間</span>
+                    </div>
+                    <div>
+                      <span>（回答：<?php echo $dbCompanyData['user_data']['COUNT(over_time)']; ?>件）</span>
+                    </div>
+                  </div>
+                </div>
+                <div class="params">
+                  <div class="item">
+                    <div class="item-name">
+                      <?php echo $dbCompanyRatings[10]['name']; ?>
+                      <div class="param">
+                        <span class="value"><?php echo round($dbCompanyRatings[10]['AVG(rating)'], 1)*20; ?></span><span class="percent"> %</span>
+                      </div>
+                    </div>
+                    <span class="bar_rating" data-rate="<?php echo round($dbCompanyRatings[10]['AVG(rating)'], 1)*20; ?>"></span>
                   </div>
                   <div>
-                    <span>（回答：<?php echo $dbCompanyData['user_data']['COUNT(anual_total_salary)']; ?>件）</span>
-                  </div>
-                </div>
-                <div class="col-6">
-                  <div class="title">
-                    平均残業時間<br>（月間）
-                  </div>
-                  <div class="mb-2">
-                    <span class="param"><?php echo round($dbCompanyData['user_data']['AVG(over_time)'], 0); ?></span>
-                    <span> 時間</span>
+                    <div class="item-name">
+                      <?php echo $dbCompanyRatings[8]['name']; ?>
+                      <div class="param">
+                        <span class="value"><?php echo round($dbCompanyRatings[8]['AVG(rating)'], 1)*20; ?></span><span class="percent"> %</span>
+                      </div>
+                    </div>
+                    <span class="bar_rating" data-rate="<?php echo round($dbCompanyRatings[8]['AVG(rating)'], 1)*20; ?>"></span>
                   </div>
                   <div>
-                    <span>（回答：<?php echo $dbCompanyData['user_data']['COUNT(over_time)']; ?>件）</span>
+                    <div class="item-name">
+                      <?php echo $dbCompanyRatings[9]['name']; ?>
+                      <div class="param">
+                        <span class="value"><?php echo round($dbCompanyRatings[9]['AVG(rating)'], 1)*20; ?></span><span class="percent"> %</span>
+                      </div>
+                    </div>
+                    <span class="bar_rating" data-rate="<?php echo round($dbCompanyRatings[9]['AVG(rating)'], 1)*20; ?>"></span>
+                  </div>
+                  <div>
+                    <div class="item-name">
+                      <?php echo $dbCompanyRatings[7]['name']; ?>
+                      <div class="param">
+                        <span class="value"><?php echo round($dbCompanyRatings[7]['AVG(rating)'], 1)*20; ?></span><span class="percent"> %</span>
+                      </div>
+                    </div>
+                    <span class="bar_rating" data-rate="<?php echo round($dbCompanyRatings[7]['AVG(rating)'], 1)*20; ?>"></span>
                   </div>
                 </div>
               </div>
-              <div class="params">
-                <div class="item">
-                  <div class="item-name">
-                    <?php echo $dbCompanyRatings[10]['name']; ?>
-                    <div class="param">
-                      <span class="value"><?php echo round($dbCompanyRatings[10]['AVG(rating)'], 1)*20; ?></span><span class="percent"> %</span>
-                    </div>
-                  </div>
-                  <span class="bar_rating" data-rate="<?php echo round($dbCompanyRatings[10]['AVG(rating)'], 1)*20; ?>"></span>
-                </div>
-                <div>
-                  <div class="item-name">
-                    <?php echo $dbCompanyRatings[8]['name']; ?>
-                    <div class="param">
-                      <span class="value"><?php echo round($dbCompanyRatings[8]['AVG(rating)'], 1)*20; ?></span><span class="percent"> %</span>
-                    </div>
-                  </div>
-                  <span class="bar_rating" data-rate="<?php echo round($dbCompanyRatings[8]['AVG(rating)'], 1)*20; ?>"></span>
-                </div>
-                <div>
-                  <div class="item-name">
-                    <?php echo $dbCompanyRatings[9]['name']; ?>
-                    <div class="param">
-                      <span class="value"><?php echo round($dbCompanyRatings[9]['AVG(rating)'], 1)*20; ?></span><span class="percent"> %</span>
-                    </div>
-                  </div>
-                  <span class="bar_rating" data-rate="<?php echo round($dbCompanyRatings[9]['AVG(rating)'], 1)*20; ?>"></span>
-                </div>
-                <div>
-                  <div class="item-name">
-                    <?php echo $dbCompanyRatings[7]['name']; ?>
-                    人間関係の満足度
-                    <div class="param">
-                      <span class="value"><?php echo round($dbCompanyRatings[7]['AVG(rating)'], 1)*20; ?></span><span class="percent"> %</span>
-                    </div>
-                  </div>
-                  <span class="bar_rating" data-rate="<?php echo round($dbCompanyRatings[7]['AVG(rating)'], 1)*20; ?>"></span>
-                </div>
-              </div>
-            </section>
-          </div>
+            </div>
+          </section>
+        </div>
+
+        <div class="l-company-main">
 
           <div class="page-content">
-            <section>
+            <section class="p-company-section">
               <h2>年収データ</h2>
               <p>回答者の平均年収：<?php echo round($dbCompanyData['user_data']['AVG(anual_total_salary)'], 0); ?>万円</p>
               <p>回答者の年収範囲：<?php echo round($dbCompanyData['user_data']['MIN(anual_total_salary)'], 0); ?>〜<?php echo round($dbCompanyData['user_data']['MAX(anual_total_salary)'], 0); ?>万円</p>
@@ -171,20 +177,19 @@ require('head.php');
             </h2>
             
             <?php if(!empty($dbPostData)){ ?>
-              <section>
-
+              <section class="p-post">
                 <?php foreach($dbPostData as $key => $val){ ?>
-                  <div class="kutikomi-header">
+                  <div class="p-post__header">
                     <div class="user-icon">
                       <i class="gg-profile"></i>
                     </div>
   
-                    <h3>
-                      <span><?php echo $dbCompanyData['info']['name']; ?></span><br>
+                    <h3 class="p-post__header__title">
+                      <span class="p-post__header__company"><?php echo $dbCompanyData['info']['name']; ?></span><br>
                       <?php echo $val['category']; ?>
                     </h3>
   
-                    <div class="user-info">
+                    <div class="p-post__header__user-info">
                       回答者：
                       <a href="">
                         <?php echo SEX[$val['sex']]; ?>
@@ -201,34 +206,31 @@ require('head.php');
                     <p></p>
                   </div>
   
-                  <h4 class="fs-1 fw-bold"><?php echo $val['answer_item']; ?>：</h4>
+                  <h4 class="p-post__item-name"><?php echo $val['answer_item']; ?>：</h4>
                   <p><?php echo $val['answer']; ?></p>
   
-                  <span class="post-date">クチコミ投稿：<?php echo date('Y年m月', strtotime($val['a_update_date'])); ?></span>
+                  <span class="p-post__date">クチコミ投稿：<?php echo date('Y年m月', strtotime($val['a_update_date'])); ?></span>
   
-                  <div class="border-bottom mb-3"></div>
+                  <div class="border-bottom u-mb-3"></div>
   
                 <?php } ?>
 
                   
               </section>
-            <?php } ?>
+            <?php } ?>          
 
-            <section>
+            <section class="p-company-section">
               <h2>カテゴリからクチコミを探す</h2>
               <div class="kutikomi-category-list">
-                <ul>
+                <ul class="p-category-list">
                   <?php foreach($dbCategoryData as $key => $val){ ?>
-                    <li><a href="category.php?co=<?php echo $company_id.'&ca='.$val['id']; ?>" class="<?php if($category_id == $val['id']) echo'bg-blue text-white'; ?>"><?php echo $val['name']; ?>（<?php echo $val['count']['COUNT(answers.id)']; ?>件）</a></li>
+                    <li><a class="p-category-list__item<?php if($category_id == $val['id']) echo' u-bg-blue u-text-white'; ?>" href="category.php?co=<?php echo $company_id.'&ca='.$val['id']; ?>"><?php echo $val['name']; ?>（<?php echo $val['count']['COUNT(answers.id)']; ?>件）</a></li>
                   <?php } ?>
                 </ul>
               </div>
-              <p>
-                <a href="">全てのクチコミを見る</a>
-              </p>
             </section>
 
-            <section>
+            <section class="p-company-section">
               <h2>企業概要</h2>
               <div class="company-info">
                 <dl>
@@ -242,35 +244,35 @@ require('head.php');
               </div>
             </section>
           </div>
-        </div>
-  
-        
+        </div>      
       </div>
     </div>
   </main>
     
-    <footer id="footer">
-      <ul class="l-container">
+  <footer id="js-footer" class="l-footer">
+    <div class="l-container">
+      <ul class="">
         <li><a href="index.php">HOME</a></li>
         <li><a href="">ご利用案内</a></li>
         <li><a href="">プライバシーポリシー</a></li>
         <li><a href="">サイトマップ</a></li>
         <li><a href="">お問い合わせ</a></li>
       </ul>
+    </div>
 
-      <span class="copyright">
-        Copyright © ヤクカイ. All Rights Reserved.
-      </span>
-    </footer>
+    <span class="l-footer__copyright">
+      Copyright © ヤクカイ. All Rights Reserved.
+    </span>
+  </footer>
     
     <!-- jQuery読み込み -->
-    <script src="js/vender/jquery-3.5.1.min.js"></script>
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
     <!-- Chart.js読み込み -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.1.0/chart.min.js" integrity="sha512-RGbSeD/jDcZBWNsI1VCvdjcDULuSfWTtIva2ek5FtteXeSjLfXac4kqkDRHVGf1TwsXCAqPTF7/EYITD0/CTqw==" crossorigin="anonymous"></script>
     <script>
       $(function(){
         // フッターを最下部に固定
-        var $ftr = $('#footer');
+        var $ftr = $('#js-footer');
         if( window.innerHeight > $ftr.offset().top + $ftr.outerHeight() ){
           //ウインドウ内コンテンツ部分の高さ　＞　普通に表示した時のコンテンツ部分左上からフッターまで高さ + フッターのボーダー外側の高さ
           $ftr.attr({
