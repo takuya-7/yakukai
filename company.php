8b44954f18a9eba3bbf16ec2263bb9fce86e5fea
takuya-7 <ts.procd@gmail.com>
2021-06-22 22:51:43 +0900

    不要なdivタグ削除
    保守性向上のため

--- company.php
+++ company.php
@@ -143,116 +143,112 @@ require('head.php');
         </div>
 
         <div class="l-company-main">
+          <section class="p-company-section">
+            <h2>年収データ</h2>
+            <p>回答者の平均年収：<?php echo round($dbCompanyData['user_data']['AVG(anual_total_salary)'], 0); ?>万円</p>
+            <p>回答者の年収範囲：<?php echo round($dbCompanyData['user_data']['MIN(anual_total_salary)'], 0); ?>〜<?php echo round($dbCompanyData['user_data']['MAX(anual_total_salary)'], 0); ?>万円</p>
+            <p>回答者数：<?php echo $dbCompanyData['user_data']['COUNT(anual_total_salary)']; ?>人</p>
+            <p>回答者の平均年齢：<?php echo date('Y')-round($dbCompanyData['user_data']['AVG(users.birth_year)']); ?>歳</p>
+          </section>
 
-          <div class="page-content">
-            <section class="p-company-section">
-              <h2>年収データ</h2>
-              <p>回答者の平均年収：<?php echo round($dbCompanyData['user_data']['AVG(anual_total_salary)'], 0); ?>万円</p>
-              <p>回答者の年収範囲：<?php echo round($dbCompanyData['user_data']['MIN(anual_total_salary)'], 0); ?>〜<?php echo round($dbCompanyData['user_data']['MAX(anual_total_salary)'], 0); ?>万円</p>
-              <p>回答者数：<?php echo $dbCompanyData['user_data']['COUNT(anual_total_salary)']; ?>人</p>
-              <p>回答者の平均年齢：<?php echo date('Y')-round($dbCompanyData['user_data']['AVG(users.birth_year)']); ?>歳</p>
-            </section>
-
-            <!-- <section>
-              <h2>処方せん処理枚数</h2>
-              <p>回答者の平均枚数（1日8時間あたり）：枚</p>
-              <p>回答者の枚数範囲：〜枚</p>
-            </section> -->
-            
-            <h2>Pick up クチコミ</h2>
-            
-            <?php foreach($dbCategoryData as $key => $category){ ?>
-              <?php $dbPickUpPosts = getPickUpPosts($company_id, $category['id']); ?>
-              <?php if(!empty($dbPickUpPosts)){ ?>
-                <section class="p-post">
-                  <?php foreach($dbPickUpPosts as $key => $val){ ?>
-                    <?php if(!empty($val['answer'])){ ?>
-                      <div class="p-post__header">
-                        <div class="user-icon">
-                          <i class="gg-profile"></i>
-                        </div>
-      
-                        <h3 class="p-post__header__title">
-                          <span class="p-post__header__company"><?php echo $dbCompanyData['info']['name']; ?></span><br>
-                          <?php echo $val['category']; ?>
-                        </h3>
-                        
-                        <div class="p-post__header__user-info">
-                          回答者：
-                          <a href="post.php<?php echo '?c_id='.$company_id.'&p_id='.$val['post_id']; ?>">
-                            <?php echo SEX[$val['sex']]; ?>
-                            <?php echo '、'.ENTRY_TYPE[$val['entry_type']]; ?>
-                            <?php echo '、'.REGISTRATION[$val['registration']].'（回答時）'; ?>
-                            <?php echo '、在籍'.getYearDiff($val['a_update_date'], $val['entry_date']).'年'; ?>
-                          </a>
-                        </div>
-  
-                        <div class="px-3">
-                          <span class="heart5_rating" data-rate="<?php echo $val['rating']; ?>"></span>
-                          <span class="fs-3 ms-1">
-                            <?php echo number_format($val['rating'], 1); ?>
-                          </span>
-                        </div>
-                        
+          <!-- <section>
+            <h2>処方せん処理枚数</h2>
+            <p>回答者の平均枚数（1日8時間あたり）：枚</p>
+            <p>回答者の枚数範囲：〜枚</p>
+          </section> -->
+          
+          <h2>Pick up クチコミ</h2>
+          
+          <?php foreach($dbCategoryData as $key => $category){ ?>
+            <?php $dbPickUpPosts = getPickUpPosts($company_id, $category['id']); ?>
+            <?php if(!empty($dbPickUpPosts)){ ?>
+              <section class="p-post">
+                <?php foreach($dbPickUpPosts as $key => $val){ ?>
+                  <?php if(!empty($val['answer'])){ ?>
+                    <div class="p-post__header">
+                      <div class="user-icon">
+                        <i class="gg-profile"></i>
                       </div>
-  
-                      <h4 class="p-post__item-name"><?php echo $val['answer_item']; ?>：</h4>
-                      <p><?php echo $val['answer']; ?></p>
-      
-                      <div class="p-answer-bottom">
-                        <span class="c-good">
-                          <span class="c-good__comment">参考になった！</span>
-                          <i class="fa fa-thumbs-up c-good__icon
-                          <?php
-                            if(isset($_SESSION['user_id'])){
-                              echo ' js-click-good';
-                              if(isGood($_SESSION['user_id'], $val['answer_id'])) echo ' is-good-active';
-                            }
-                          ?>" aria-hidden="true" data-answer_id="<?php echo sanitize($val['answer_id']);?>" ></i>
-                          <span class="c-good__count js-good-count"><?php echo getGoodCount($val['answer_id']); ?></span>
+    
+                      <h3 class="p-post__header__title">
+                        <span class="p-post__header__company"><?php echo $dbCompanyData['info']['name']; ?></span><br>
+                        <?php echo $val['category']; ?>
+                      </h3>
+                      
+                      <div class="p-post__header__user-info">
+                        回答者：
+                        <a href="post.php<?php echo '?c_id='.$company_id.'&p_id='.$val['post_id']; ?>">
+                          <?php echo SEX[$val['sex']]; ?>
+                          <?php echo '、'.ENTRY_TYPE[$val['entry_type']]; ?>
+                          <?php echo '、'.REGISTRATION[$val['registration']].'（回答時）'; ?>
+                          <?php echo '、在籍'.getYearDiff($val['a_update_date'], $val['entry_date']).'年'; ?>
+                        </a>
+                      </div>
+
+                      <div class="px-3">
+                        <span class="heart5_rating" data-rate="<?php echo $val['rating']; ?>"></span>
+                        <span class="fs-3 ms-1">
+                          <?php echo number_format($val['rating'], 1); ?>
                         </span>
-                        <span class="p-post__date">クチコミ投稿：<?php echo date('Y年m月', strtotime($val['a_update_date'])); ?></span>
                       </div>
-      
-                      <div class="border-bottom u-mb-3"></div>
-                    <?php } ?>
+                      
+                    </div>
+
+                    <h4 class="p-post__item-name"><?php echo $val['answer_item']; ?>：</h4>
+                    <p><?php echo $val['answer']; ?></p>
+    
+                    <div class="p-post__bottom">
+                      <span class="c-good">
+                        <span class="c-good__comment">参考になった！</span>
+                        <i class="fa fa-thumbs-up c-good__icon
+                        <?php
+                          if(isset($_SESSION['user_id'])){
+                            echo ' js-click-good';
+                            if(isGood($_SESSION['user_id'], $val['answer_id'])) echo ' is-good-active';
+                          }
+                        ?>" aria-hidden="true" data-answer_id="<?php echo sanitize($val['answer_id']);?>" ></i>
+                        <span class="c-good__count js-good-count"><?php echo getGoodCount($val['answer_id']); ?></span>
+                      </span>
+                      <span class="p-post__date">クチコミ投稿：<?php echo date('Y年m月', strtotime($val['a_update_date'])); ?></span>
+                    </div>
+    
+                    <div class="border-bottom u-mb-3"></div>
                   <?php } ?>
+                <?php } ?>
 
-                  <div class="category-btn">
-                    <a href="category.php?co=<?php echo $company_id.'&ca='.$category['id']; ?>"><span class="fw-bold">「<?php echo $category['name']; ?>」</span> <br><span class="u-fs-08 category-append">のクチコミをもっと見る（<?php echo $category['count']['COUNT(answers.id)']; ?>件）</span></a>
-                  </div>
-                    
-                </section>
-              <?php } ?>
-            <?php } ?>            
+                <div class="category-btn">
+                  <a href="category.php?co=<?php echo $company_id.'&ca='.$category['id']; ?>"><span class="fw-bold">「<?php echo $category['name']; ?>」</span> <br><span class="u-fs-08 category-append">のクチコミをもっと見る（<?php echo $category['count']['COUNT(answers.id)']; ?>件）</span></a>
+                </div>
+                  
+              </section>
+            <?php } ?>
+          <?php } ?>            
 
-            <section class="p-company-section">
-              <h2>カテゴリからクチコミを探す</h2>
-              <div class="kutikomi-category-list">
-                <ul class="p-category-list">
-                  <?php foreach($dbCategoryData as $key => $val){ ?>
-                    <li><a class="p-category-list__item" href="category.php?co=<?php echo $company_id.'&ca='.$val['id']; ?>"><?php echo $val['name']; ?>（<?php echo $val['count']['COUNT(answers.id)']; ?>件）</a></li>
-                  <?php } ?>
-                </ul>
-              </div>
-            </section>
+          <section class="p-company-section">
+            <h2>カテゴリからクチコミを探す</h2>
+            <div class="kutikomi-category-list">
+              <ul class="p-category-list">
+                <?php foreach($dbCategoryData as $key => $val){ ?>
+                  <li><a class="p-category-list__item" href="category.php?co=<?php echo $company_id.'&ca='.$val['id']; ?>"><?php echo $val['name']; ?>（<?php echo $val['count']['COUNT(answers.id)']; ?>件）</a></li>
+                <?php } ?>
+              </ul>
+            </div>
+          </section>
 
-            <section class="p-company-section">
-              <h2>企業概要</h2>
-              <div class="company-info">
-                <dl>
-                  <dt>企業名</dt>
-                  <dd><?php echo $dbCompanyData['info']['name']; ?></dd>
-                  <dt>フリガナ</dt>
-                  <dd><?php echo $dbCompanyData['info']['furigana']; ?></dd>
-                  <dt>本社所在地</dt>
-                  <dd><?php echo $dbCompanyData['info']['prefecture_name'].$dbCompanyData['info']['city_name'].$dbCompanyData['info']['street_number']; ?></dd>
-                </dl>
-              </div>
-            </section>
-          </div>
+          <section class="p-company-section">
+            <h2>企業概要</h2>
+            <div class="company-info">
+              <dl>
+                <dt>企業名</dt>
+                <dd><?php echo $dbCompanyData['info']['name']; ?></dd>
+                <dt>フリガナ</dt>
+                <dd><?php echo $dbCompanyData['info']['furigana']; ?></dd>
+                <dt>本社所在地</dt>
+                <dd><?php echo $dbCompanyData['info']['prefecture_name'].$dbCompanyData['info']['city_name'].$dbCompanyData['info']['street_number']; ?></dd>
+              </dl>
+            </div>
+          </section>
         </div>
-  
         
       </div>
     </div>
